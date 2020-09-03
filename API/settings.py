b52276ab5cd67c3e5b7a5699ef6fffdff7d791ee
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-09-04 11:01:12 +1200

    update index

--- API/settings.py
+++ API/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authenticate.apps.AuthenticateConfig',
-    'fijiApi.apps.FijiapiConfig'
+    'fijiApi.apps.FijiapiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
