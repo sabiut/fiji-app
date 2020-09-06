9c94563cb78327be684b80670be54719aff3a8db
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-09-06 16:43:30 +1200

    update setting

--- API/settings.py
+++ API/settings.py
@@ -54,8 +54,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'API.urls'
+
+CORS_ALLOWED_ORIGINS = [
+   "http://ec2-100-25-143-66.compute-1.amazonaws.com/",
+]
 
 TEMPLATES = [
     {
