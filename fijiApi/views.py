62e9234201bc24a50dc08cbb91a3a93586e2104e
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-09-08 02:44:14 +1200

    update views

--- fijiApi/views.py
+++ fijiApi/views.py
@@ -12,27 +12,27 @@ class ConfederancyViewSet(viewsets.ModelViewSet):
     serializer_class = ConfederacySerializer
     queryset = Confederacy.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class ProvincesViewSet(viewsets.ModelViewSet):
     serializer_class = ProvincesSerializer
     queryset = Provinces.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class DistrictsViewSet(viewsets.ModelViewSet):
     serializer_class = DistrictsSerializer
     queryset = Districts.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class VillageViewSet(viewsets.ModelViewSet):
     serializer_class = VillagesSerializer
     queryset = Villages.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
