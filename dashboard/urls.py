23473dd095c174ee878862a02b7a614c622695c4
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-10-25 12:48:21 +1300

    update data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,8 @@ from .views import Dashboard
 from . import views
 
 urlpatterns = [
-    path('dashboard/', Dashboard.as_view()),
-    path('getdashboard/', views.getdashboard, name="getdashboard"),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('fiji_confederacy/', views.getdashboard, name="fiji_confederacy"),
     path('district/', views.districts, name="district"),
     path('village/', views.village,name="village")
 
